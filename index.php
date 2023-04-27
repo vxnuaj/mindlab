c4a041ccede3dfc7032528c079ab052cd8cbdacf
vxnuaj <jv.100420@gmail.com>
2023-04-26 23:49:12 -0400

    php now

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <title>mindlab</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="background-image">
+            <div class="navbar">
+                    <a href="/index.html" id="mindlab_logo">mindlab.</a>
+                    <ul> 
+                        <div class="comdropcontainer"></div>
+                        <li><a href="/community.html">community.</a></li>
+                            <ul class="communitydropdown">
+                                <br>
+                                <li><a href="">forums.</a></li>
+                                <br><br>
+                                <li><a href="">growthlogs.</a></li>
+                            </ul>
+                        <li><a href="https://www.book.mindlab.lol/">mindbook.</a></li>
+                        <li><a href="/beginthequest.html">begin the quest.</a></li>
+                    </ul>
+            </div>
+            <div class="content">
+                <h1>a platform to <strong id="gradtext">build</strong>
+                    your <strong id="gradtext">mindset</strong> to
+                    <strong id="gradtext">enhance</strong> your <strong id="gradtext">life.</strong></h1>
+            </div>
+            <button id="mainbuttons" type="learnmore"><span></span>learn more.</button>
+            <button id="mainbuttons" type="beginthequest"><span></span>begin the quest.</button>
+            <img src="images/heart-dynamic-gradient.png" id="heart">
+        </div>
+    </body>
+</html>
